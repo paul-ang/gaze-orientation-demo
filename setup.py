67a99ffd17840c0dc186dea7b8084ed22583d059
Paul Ang <victor_angsp@hotmail.com>
2023-01-26 22:35:24 +1100

    added requirements for the setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ import os
 import setuptools
 
 here = os.path.abspath(os.path.dirname(__file__))
-try:
-    with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-        REQUIRED = f.read().split('\n')
-except:
-    REQUIRED = []
+
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    REQUIRED = f.read().split('\n')
 
 setuptools.setup(
     name="gaze_orientation_demo",
-    version="3",
+    version="4",
     author="Paul Ang",
     long_description="A demo of gaze orientation estimation.",
     description="A demo of gaze orientation estimation.",
