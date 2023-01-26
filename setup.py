215be9e224e2f63267d8f20334689352a714a481
Paul Ang <victor_angsp@hotmail.com>
2023-01-26 23:03:03 +1100

    updated numpy version to lower

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 
 setuptools.setup(
     name="gaze_orientation_demo",
-    version="4",
+    version="5",
     author="Paul Ang",
     long_description="A demo of gaze orientation estimation.",
     description="A demo of gaze orientation estimation.",
     packages=["gaze_orientation_demo"],
-    install_requires=['numpy==1.21.6', 'mediapipe==0.9.0.1', 'opencv-python==4.7.0.68'],
+    install_requires=['numpy==1.21.0', 'mediapipe==0.9.0.1', 'opencv-python==4.7.0.68'],
     python_requires='>=3.7.0'
 )
