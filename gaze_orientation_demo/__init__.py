6271d8d9c3a39fe3c7f1a6d01d000d7d18846958
Paul Ang <victor_angsp@hotmail.com>
2023-01-26 21:32:30 +1100

    Update __init__.py

--- gaze_orientation_demo/__init__.py
+++ gaze_orientation_demo/__init__.py
@@ -1,3 +1,1 @@
 from .gaze_estimation import GazeEstimation
-from .estimator.head_pose import HeadPose
-from .estimator.iris_pose import IrisPose
---
+++ gaze_orientation_demo/estimator/__init__.py
@@ -0,0 +1,2 @@
+from .head_pose import HeadPose
+from .iris_pose import IrisPose
